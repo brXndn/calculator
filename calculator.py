f9808e1bf74b11cf9983879993ce8d2af0668cef
brXndn <brendenucol70@gmail.com>
2025-02-04 20:12:47 -0700

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,24 +1,5 @@
 print("Welcome to the calculator app!")
 
-operation = input("What operation would you like to do? (Add, Subtract, Multiply, Divide, Exponent").title().strip()
-
-firstNum = input("Enter a first number): ")
-secondNum = input("Enter a second number): ")
-
-if operation == "Add":
-  print(f"{firstNum} + {secondNum} = {add(firstNum, secondNum)}")
-elif operation == "Subtract":
-  print(f"{firstNum} - {secondNum} = {subtract(firstNum, secondNum)}")
-elif operation == "Multiply":
-  print(f"{firstNum} * {secondNum} = {multiply(firstNum, secondNum)}")
-elif operation == "Divide":
-  print(f"{firstNum} / {secondNum} = {divide(firstNum, secondNum)}")
-elif operation == "Exponent":
-  print(f"{firstNum} ^ {secondNum} = {power(firstNum, secondNum)}")
-else:
-  print("Invalid operation. Please try again.")
-
-
 def add(x, y):
   return x + y
 
@@ -33,3 +14,22 @@ def divide(x, y):
 
 def power(x, y):
   return x ** y
+
+
+operation = input("What operation would you like to do? (Add, Subtract, Multiply, Divide, Exponent").title().strip()
+
+firstNum = int(input("Enter a first number): "))
+secondNum = int(input("Enter a second number): "))
+
+if operation == "Add":
+  print(f"{firstNum} + {secondNum} = {add(firstNum, secondNum)}")
+elif operation == "Subtract":
+  print(f"{firstNum} - {secondNum} = {subtract(firstNum, secondNum)}")
+elif operation == "Multiply":
+  print(f"{firstNum} * {secondNum} = {multiply(firstNum, secondNum)}")
+elif operation == "Divide":
+  print(f"{firstNum} / {secondNum} = {divide(firstNum, secondNum)}")
+elif operation == "Exponent":
+  print(f"{firstNum} ^ {secondNum} = {power(firstNum, secondNum)}")
+else:
+  print("Invalid operation. Please try again.")
